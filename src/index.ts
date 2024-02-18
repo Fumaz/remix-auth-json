9e3f80b27b4b6a3fba5943287e85be3291696e7f
Fumaz <mail@fumaz.dev>
2024-02-18 15:05:41 +0100

    form strategy

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,59 @@
-import { SessionStorage } from "@remix-run/server-runtime";
-import {
-  AuthenticateOptions,
-  Strategy,
-  StrategyVerifyCallback,
-} from "remix-auth";
+import { AppLoadContext, SessionStorage } from "@remix-run/server-runtime";
+import { AuthenticateOptions, Strategy } from "remix-auth";
 
-/**
- * This interface declares what configuration the strategy needs from the
- * developer to correctly work.
- */
-export interface MyStrategyOptions {
-  something: "You may need";
+export interface FormStrategyVerifyParams {
+  json: any;
+  context?: AppLoadContext;
 }
 
-/**
- * This interface declares what the developer will receive from the strategy
- * to verify the user identity in their system.
- */
-export interface MyStrategyVerifyParams {
-  something: "Dev may need";
-}
-
-export class MyStrategy<User> extends Strategy<User, MyStrategyVerifyParams> {
-  name = "change-me";
-
-  constructor(
-    options: MyStrategyOptions,
-    verify: StrategyVerifyCallback<User, MyStrategyVerifyParams>
-  ) {
-    super(verify);
-    // do something with the options here
-  }
+export class FormStrategy<User> extends Strategy<
+  User,
+  FormStrategyVerifyParams
+> {
+  name = "json";
 
   async authenticate(
     request: Request,
     sessionStorage: SessionStorage,
     options: AuthenticateOptions
   ): Promise<User> {
-    return await this.failure(
-      "Implement me!",
-      request,
-      sessionStorage,
-      options
-    );
-    // Uncomment me to do a success response
-    // this.success({} as User, request, sessionStorage, options);
+    const json = await request.json();
+
+    try {
+      const user = await this.verify({
+        json,
+        context: options.context,
+      });
+
+      return this.success(user, request, sessionStorage, options);
+    } catch (error) {
+      if (error instanceof Error) {
+        return await this.failure(
+          error.message,
+          request,
+          sessionStorage,
+          options,
+          error
+        );
+      }
+
+      if (typeof error === "string") {
+        return await this.failure(
+          error,
+          request,
+          sessionStorage,
+          options,
+          new Error(error)
+        );
+      }
+
+      return await this.failure(
+        "Unknown error",
+        request,
+        sessionStorage,
+        options,
+        new Error(JSON.stringify(error, null, 2))
+      );
+    }
   }
 }
